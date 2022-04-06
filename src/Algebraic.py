c74ae216492276c6bc9f604f4d5578eada9366a0
JanSafronov <Jansafronov@gmail.com>
2022-04-06 16:27:58 +0300

    Commit temporary plotting

--- src/Algebraic.py
+++ src/Algebraic.py
@@ -108,7 +108,7 @@ class VectorSpace(Set, Generic[F, V]):
             raise ValueError("The argument is not a scalar")
         return VectorSpace(self.field, [self.basis[i] * scalar for i in range(self.dimension)])
 
-    def ParametricEquation(self) -> Callable[[tuple[V, ...]], V]:
+    def parametricEquation(self) -> Callable[[tuple[V, ...]], V]:
         """
         :return: A parametric equation for the vector space
         """
@@ -123,7 +123,7 @@ class AffineSpace():
     """
     An affine space is a vector space with a point
     """
-    def __init__(self, vector_space: VectorSpace, point: Vector):
+    def __init__(self, vector_space: VectorSpace, point: Union[Vector, List[F]]):
         """
         :param vector_space: A vector space
         :param point: A point in the affine space
@@ -179,11 +179,19 @@ class AffineSpace():
             raise ValueError("The affine spaces are not in the same vector space")
         return AffineSpace(self.vector_space, self.point - other.point)
 
+    def parametricEquation(self) -> Callable[[tuple[V, ...]], V]:
+        """
+        :return: A parametric equation for the affine space
+        """
+        mcrossp = np.cross(self.vector_space.basis[0], self.vector_space.basis[1])
+        dim = len(mcrossp)
+        return lambda Λ: sum((mcrossp[:dim - 1] / mcrossp[dim - 1])[i] * (Λ[i] + self.point[i]) for i in range(dim - 1))
+
 class AffineVariety(Generic[F, V]):
     """ 
-    An affine variety is a set of solutions of a field of a system of polynomial equations with coefficients in the field
+    An affine variety is a set of solutions of a system of polynomial equations with coefficients in the field
     """
-    def __init__(self, field: F, equations: List[Callable[[tuple[V, ...]], F]]):
+    def __init__(self, field: F, equations: set[Callable[[tuple[V, ...]], F]]):
         """
         :param field: A field
         :param equations: A list of equations
@@ -192,10 +200,10 @@ class AffineVariety(Generic[F, V]):
         self.equations = equations
 
     def __str__(self):
-        return "AffineVariety(field = {}, equations = {})".format(self.field, self.equations)
+        return "A = {} affine variety with polynomial equations \n{} over field F = {}".format(self.vectors, self.equations, self.field)
 
     def __repr__(self):
-        return self.__str__()
+        return "AffineVariety(field = {}, equations = {})".format(self.field, self.equations)
 
     def __eq__(self, other):
         return self.field == other.field and self.equations == other.equations
@@ -230,11 +238,41 @@ class AffineVariety(Generic[F, V]):
             raise ValueError("The affine varieties are not in the same field")
         if self.dimension != other.dimension:
             raise ValueError("The affine varieties are not the same dimension")
-        return AffineVariety(self.field, [self.equations[i] + other.equations[i] for i in range(self.dimension)])
+        return AffineVariety(self.field, self.equations | other.equations)
+
+    def __or__(self, other: "AffineVariety") -> "AffineVariety":
+        """
+        :param other: An affine variety
+        :return: The union of the two affine varieties
+        """
+        if self.field != other.field:
+            raise ValueError("The affine varieties are not in the same field")
+        if self.dimension != other.dimension:
+            raise ValueError("The affine varieties are not the same dimension")
+        return AffineVariety(self.field, self.equations | other.equations)
+
+    def __and__(self, other: "AffineVariety") -> "AffineVariety":
+        """
+        :param other: An affine variety
+        :return: The intersection of the two affine varieties
+        """
+        if self.field != other.field:
+            raise ValueError("The affine varieties are not in the same field")
+        if self.dimension != other.dimension:
+            raise ValueError("The affine varieties are not the same dimension")
+        return AffineVariety(self.field, (lambda T: self.equations[i](T) * other.equations[i](T) for i in range(self.dimension)))
+
+    def solutions(self) -> set[tuple[V, ...]]:
+        """
+        :param affine_space: An affine space
+        :return: The solutions of the affine variety in the affine space
+        """
+        print(np.roots(self.equations))
+        return set(filter(lambda T: self.equations[i](T) == self.field.zero for i in range(self.dimension)))
 
 class RationalFunction(Generic[F, V]):
     """
-    A rational function is a polynomial with coefficients in a field
+    A rational function with coefficients in a field is a quotient of two polynomials 
     """
     def __init__(self, field: F, polynomial: Callable[[tuple[V, ...]], F]):
         """
@@ -292,6 +330,66 @@ class RationalFunction(Generic[F, V]):
             raise ValueError("The rational functions are not in the same field")
         return RationalFunction(self.field, self.polynomial - other.polynomial)
 
+class Monomial(Generic[F, V]):
+    """
+    A monomial is a rational function with a single variable
+    """
+    def __init__(self, field: F, variable: V):
+        """
+        :param field: A field
+        :param variable: A variable
+        """
+        self.field = field
+        self.variable = variable
+
+    def __str__(self):
+        return "Monomial(field = {}, variable = {})".format(self.field, self.variable)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __eq__(self, other):
+        return self.field == other.field and self.variable == other.variable
+
+    def __hash__(self):
+        return hash(self.__str__())
+
+    def __contains__(self, affine_variety: AffineVariety):
+        return affine_variety in self.variable
+
+    def __iter__(self):
+        return iter(self.variable)
+
+    def __len__(self):
+        return len(self.variable)
+
+    def __getitem__(self, index):
+        return self.variable[index]
+
+    def __setitem__(self, index, affine_variety):
+        self.variable[index] = affine_variety
+
+    def __delitem__(self, index):
+        del self.variable[index]
+
+    def __add__(self, other: "Monomial") -> "Monomial":
+        """
+        :param other: A monomial
+        :return: The sum of the two monomials
+        """
+        if self.field != other.field:
+            raise ValueError("The monomials are not in the same field")
+        return Monomial(self.field, self.variable + other.variable)
+
+    def __sub__(self, other: "Monomial") -> "Monomial":
+        """
+        :param other: A monomial
+        :return: The difference of the two monomials
+        """
+        if self.field != other.field:
+            raise ValueError("The monomials are not in the same field ")
+        return Monomial(self.field, self.variable - other.variable)
+
 class Ideal(Generic[F, V]):
     """
     An ideal is a set of solutions of a field of a system of polynomial equations with coefficients in the field
@@ -306,10 +404,10 @@ class Ideal(Generic[F, V]):
         self.dimension = len(equations)
 
     def __str__(self):
-        return "Ideal(field = {}, equations = {})".format(self.field, self.equations)
+        return "I = ⟨⟩(field = {}, equations = {})".format(self.field, self.equations)
 
     def __repr__(self):
-        return self.__str__()
+        return "Ideal(field = {}, equations = {})".format(self.field, self.equations)
 
     def __eq__(self, other):
         return self.field == other.field and self.equations == other.equations
@@ -345,3 +443,9 @@ class Ideal(Generic[F, V]):
         if self.dimension != other.dimension:
             raise ValueError("The ideals are not the same dimension")
         return Ideal(self.field, [self.equations[i] + other.equations[i] for i in range(self.dimension)])
+
+    def is_monomial(self) -> bool:
+        """
+        :return: True if the ideal is a monomial ideal
+        """
+        return all(self.equations[i].polynomial.is_monomial() for i in range(self.dimension))
